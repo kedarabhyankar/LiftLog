c2824c745357e84d1c9ae90d01fed04c21d87c72
Kedar Abhyankar <krabhyankar@gmail.com>
2022-01-01 15:22:05 -0800

    Initial Commit

---
+++ LiftLog/LiftLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftLogApp.swift
+//  LiftLog
+//
+//  Created by Kedar Abhyankar on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LiftLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
